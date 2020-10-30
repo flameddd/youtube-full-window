ec0f85e2acd0565b69a71d3d4afeb3c78d836938
Jacky Hu <flameddd@gmail.com>
2020-10-31 00:00:25 +0800

    fix: GITHUB_PAGES env

--- preact.config.js
+++ preact.config.js
@@ -8,10 +8,13 @@ export default (config, env, helpers) => {
   if (process.env.GITHUB_PAGES) {
     config.output.publicPath = `/${process.env.GITHUB_PAGES}/`;
 
+    const ghEnv = process.env.GITHUB_PAGES
+      && JSON.stringify(`${process.env.GITHUB_PAGES}`);
+
     const { plugin } = helpers.getPluginsByName(config, 'DefinePlugin')[0];
     Object.assign(
       plugin.definitions,
-      { ['process.env.GITHUB_PAGES']: `${process.env.GITHUB_PAGES}` }
+      { ['process.env.GITHUB_PAGES']: ghEnv }
     );
   }
 
