5e6e635db1e06e35bb28107289cdbf66455995eb
Jacky Hu <flameddd@gmail.com>
2020-11-03 23:44:25 +0800

    feat: add purgecss

--- preact.config.js
+++ preact.config.js
@@ -1,3 +1,25 @@
+// tailwindcss purgecss
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: [
+    './src/**/*.html',
+    './src/**/*.jsx',
+    './src/**/*.js',
+  ],
+
+  // This is the function used to extract class names from your templates
+  defaultExtractor: content => {
+    // Capture as liberally as possible, including things like `h-(screen-1.5)`
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+
+    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+
+    return broadMatches.concat(innerMatches)
+  }
+})
+
+
 export default (config, env, helpers) => {
   config.node.process = true // fix "process is not defined"
 
@@ -5,6 +27,9 @@ export default (config, env, helpers) => {
   postCSSLoader.loader.options.plugins = [
     require("tailwindcss")("./tailwind.config.js"),
     require("autoprefixer"),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ].concat(postCSSLoader.loader.options.plugins);
 
   if (process.env.GITHUB_PAGES) {
